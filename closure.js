d5230607c14ed1f5cdc22a1546938e0a8bf8adfa
Cess <cessmbuguar@gmail.com>
2016-12-10 07:10:21 +0300

    a closure function example

---
+++ closure.js
@@ -0,0 +1,14 @@
+function init() {
+ name = "Batman";
+ function displayName(){
+     console.log (name);
+
+ }
+ displayName();
+
+}
+init();
+
+//var  closure = returnClosure(name)
+ //closure;
+// return closure;
